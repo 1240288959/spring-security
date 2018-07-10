4bade1343a96b09e0233b02a8422dcc1a023b2eb
TanYang <1240288959@qq.com>
2018-07-10 15:36:35 +0800

    上传sql文件
    mysql数据库生成的sql文件，包含用户表，和角色表等

---
+++ spring_security/security_demo/sql/securtiy.sql
@@ -0,0 +1,54 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: securtiy
+Target Host: localhost
+Target Database: securtiy
+Date: 2018/7/10 15:34:57
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for t_role
+-- ----------------------------
+CREATE TABLE `t_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rolename` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+CREATE TABLE `t_user` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `isAccountNonExpired` binary(255) NOT NULL,
+  `isAccountNonLocked` binary(255) NOT NULL,
+  `isCredentialsNonExpired` binary(255) NOT NULL,
+  `isEnabled` binary(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_user_role
+-- ----------------------------
+CREATE TABLE `t_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) DEFAULT NULL,
+  `role` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`),
+  KEY `role` (`role`),
+  CONSTRAINT `t_user_role_ibfk_2` FOREIGN KEY (`role`) REFERENCES `t_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `t_user_role_ibfk_1` FOREIGN KEY (`user`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `t_role` VALUES ('1', 'admin');
+INSERT INTO `t_role` VALUES ('2', 'user');
+INSERT INTO `t_user` VALUES ('1', 'ty', '123', '1', '1', '1', '1');
+INSERT INTO `t_user_role` VALUES ('1', '1', '1');
